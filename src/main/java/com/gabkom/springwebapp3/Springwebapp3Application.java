dfe0b8ca8c4ddd4203ae5aa3de74631623b39c1a
Gabriel Komarnicki <20436870+gabkomarnicki@users.noreply.github.com>
2021-08-03 13:45:16 +1000

    First commit

---
+++ src/main/java/com/gabkom/springwebapp3/Springwebapp3Application.java
@@ -0,0 +1,13 @@
+package com.gabkom.springwebapp3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springwebapp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springwebapp3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabkom/springwebapp3/Springwebapp3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabkom.springwebapp3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springwebapp3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
