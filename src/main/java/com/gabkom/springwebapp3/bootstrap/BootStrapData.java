9b688502a65261651db926837b0136baaa5a8944
Gabriel Komarnicki <20436870+gabkomarnicki@users.noreply.github.com>
2021-08-05 16:23:19 +1000

    Created BootStrapData

---
+++ src/main/java/com/gabkom/springwebapp3/bootstrap/BootStrapData.java
@@ -0,0 +1,48 @@
+package com.gabkom.springwebapp3.bootstrap;
+
+import com.gabkom.springwebapp3.domain.Author;
+import com.gabkom.springwebapp3.domain.Book;
+import com.gabkom.springwebapp3.repositories.AuthorRepository;
+import com.gabkom.springwebapp3.repositories.BookRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootStrapData implements CommandLineRunner {
+
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+
+    // Spring will perform dependency injection below for the authorRepository and bookRepository
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Author eric = new Author("Eric", "Evans");
+        Book ddd = new Book("Domain Driven Design","123123");
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+
+        // these will be saved in the H2 database
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE Development without EJB","345436362");
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+
+        System.out.println("Started in Bootstrap");
+        System.out.println("Number of books: " + bookRepository.count());
+
+
+
+    }
+}
--- src/main/java/com/gabkom/springwebapp3/domain/Book.java
+++ src/main/java/com/gabkom/springwebapp3/domain/Book.java
@@ -1,6 +1,7 @@
 package com.gabkom.springwebapp3.domain;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -16,15 +17,14 @@ public class Book {
     @ManyToMany
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
             inverseJoinColumns = @JoinColumn(name = "author_id"))
-    private Set<Author> authors;
+    private Set<Author> authors = new HashSet<>(); // initialise the property by default
 
     public Book() {
     }
 
-    public Book(String title, String isbn, Set<Author> authors) {
+    public Book(String title, String isbn) {
         this.title = title;
         this.isbn = isbn;
-        this.authors = authors;
     }
 
     public Long getId() {
