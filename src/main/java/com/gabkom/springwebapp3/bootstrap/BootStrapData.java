b6e7bb93b963f2a11d14602ec4fcccb2dbb58ca8
Gabriel Komarnicki <20436870+gabkomarnicki@users.noreply.github.com>
2021-08-06 21:22:46 +1000

    Inserted book values to publisher

--- src/main/java/com/gabkom/springwebapp3/bootstrap/BootStrapData.java
+++ src/main/java/com/gabkom/springwebapp3/bootstrap/BootStrapData.java
@@ -29,36 +29,44 @@ public class BootStrapData implements CommandLineRunner {
 
         Author eric = new Author("Eric", "Evans");
         Book ddd = new Book("Domain Driven Design","123123");
-        Publisher abc = new Publisher("First Publishing Company", "123 Main St", "Springfield", "MA", "123456");
+//        Publisher publisher = new Publisher("First Publishing Company", "123 Main St", "Springfield", "MA", "123456");
 
-        // Another way of insert values
-//        Publisher abc = new Publisher();
-//        abc.setName("First Publishing Company");
-//        abc.setCity("Springfield");
-//        abc.setState("MA");
-//
-//        publisherRepository.save(abc);
+        Publisher publisher = new Publisher();
+        publisher.setName("First Publishing Company");
+        publisher.setCity("Springfield");
+        publisher.setState("MA");
+
+        publisherRepository.save(publisher);
 
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
-        abc.getAuthors().add(eric);
+
+        ddd.setPublisher(publisher);
+        publisher.getBooks().add(ddd);
+        
+//        publisher.getAuthors().add(eric);
 
         // these will be saved in the H2 database
         authorRepository.save(eric);
         bookRepository.save(ddd);
-        publisherRepository.save(abc);
+        publisherRepository.save(publisher);
 
         Author rod = new Author("Rod", "Johnson");
         Book noEJB = new Book("J2EE Development without EJB","345436362");
         rod.getBooks().add(noEJB);
         noEJB.getAuthors().add(rod);
 
+        noEJB.setPublisher(publisher);
+        publisher.getBooks().add(noEJB);
+
         authorRepository.save(rod);
         bookRepository.save(noEJB);
+        publisherRepository.save(publisher);
 
         System.out.println("Started in Bootstrap");
         System.out.println("Number of books: " + bookRepository.count());
         System.out.println("Number of publishers: " + publisherRepository.count());
+        System.out.println("Publisher number of books: " + publisher.getBooks().size());
 
 
     }
